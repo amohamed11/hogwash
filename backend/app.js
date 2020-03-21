1d6eecce92fa96f84da35596d242c4bd94484125
Anas Mohamed <amohamed@ualberta.ca>
2020-03-21 15:24:11 -0600

    updated node packages, changed express to use pug

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
